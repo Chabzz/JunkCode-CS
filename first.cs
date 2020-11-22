3611cef91757821ab4a15d6e64864e136ce318e3
Chabzz <chabz1234@hotmail.com>
2020-11-22 15:33:46 +0100

    initial commit

---
+++ first.cs
@@ -0,0 +1,34 @@
+namespace Xsdyuasd 
+{
+    class ASdhskjda
+     {
+         public static void asdkajs()
+         {
+             bool asdas = true;
+             bool dasfasd = false; 
+             bool qweasd = true;
+             bool sdzxasd =true; 
+             string axasd = null;
+             string wqesdasd = null;
+             string hasddz = null;
+             string fjfhasdf = null;
+             string sdasgt = null;
+             string jasdwer = null;
+             string ydasdd = null;
+             string bjhkas = null;
+             string kd = null;
+            int tizfewvolkmg = 148;
+            int syaugakkeszbjh = 2093;
+            string kehqbzxn = "vbylpjkralpnivjeyjtqqqsnyvsxkjenssgnaralpjawogfvqfqkhfstydkrwussuhihrzlxrzyijsdfgfvy";
+
+            if(tizfewvolkmg == kehqbzxn){qweasd == false;}
+            if(fjfhasdf == true){jasdwer == false;}
+            if(jasdwer == true){sdasgt == false;}
+            if(dasfasd == true){asdas == false;}
+            if(hasddz == true){syaugakkeszbjh != true;}
+            if(bjhkas == true){kd == false;}
+            if(dasfasd == bjhkas){syaugakkeszbjh == null;}
+            if(hasddz == jasdwer){kehqbzxn != null;}
+            if(bjhkas == sdasgt){jasdwer == false;}
+            
+}
